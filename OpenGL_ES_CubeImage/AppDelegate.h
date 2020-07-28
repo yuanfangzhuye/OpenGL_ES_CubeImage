81a25bda1014bd1ecb8e2911648d4a1d6f7b1695
chao.li <chao.li@team-lab.cn>
2020-07-28 10:48:12 +0800

    Initial Commit

---
+++ OpenGL_ES_CubeImage/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  OpenGL_ES_CubeImage
+//
+//  Created by tlab on 2020/7/28.
+//  Copyright © 2020 yuanfangzhuye. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ OpenGL_ES_CubeImage/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  OpenGL_ES_CubeImage
+//
+//  Created by tlab on 2020/7/28.
+//  Copyright © 2020 yuanfangzhuye. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
